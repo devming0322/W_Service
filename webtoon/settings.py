78d894049f52af3b4b74d034ee5a95481955736c
devming0322 <85321935+devming0322@users.noreply.github.com>
2021-09-27 12:29:24 +0900

    Update settings.py

--- webtoon/settings.py
+++ webtoon/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'idea.apps.IdeaConfig'
+    'idea.apps.IdeaConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
